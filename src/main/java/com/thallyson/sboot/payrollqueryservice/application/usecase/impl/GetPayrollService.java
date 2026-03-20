a91b5fc26a43573d0e07d307d939b5863eed8a9d
thallysonsalazar2025 <thallyson.salazar@gmail.com>
2026-03-20 11:57:39 -0300

    Create GetPayrollService implementing GetPayrollUseCase

---
+++ src/main/java/com/thallyson/sboot/payrollqueryservice/application/usecase/impl/GetPayrollService.java
@@ -0,0 +1,7 @@
+package com.thallyson.sboot.payrollqueryservice.application.usecase.impl;
+
+import com.thallyson.sboot.payrollqueryservice.application.usecase.GetPayrollUseCase;
+
+public class GetPayrollService implements GetPayrollUseCase {
+    // Implement the required methods for GetPayrollUseCase here
+}
